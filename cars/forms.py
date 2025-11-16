693a653d8ba11abc660e8fb49331a94106832d71
Carlos Alberto <carlosssilva.filho@gmail.com>
2025-11-16 19:57:41 -0300

    VAlidação de campos

--- cars/forms.py
+++ cars/forms.py
@@ -32,3 +32,20 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+    #validação para cadastrar carros com valor minimode R$ 20.000
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value','Valor minimo deve ser de RS 20.000')
+        return value
+    
+    #validação para ano de fabricaçao minimo de 1970
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1970:
+            self.add_error('factory_year','Não é possivel cadastrar carros fabricados antes de 1970')
+        return factory_year
+
+        
+    
