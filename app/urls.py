68a723bb730b36edfe45ca871270adf653f773a2
Carlos Alberto <carlosssilva.filho@gmail.com>
2025-11-17 21:21:32 -0300

    Tela de Login e função logout

--- app/urls.py
+++ app/urls.py
@@ -4,14 +4,16 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import car_view, new_car_view
-from accounts.views import register_view
+from accounts.views import register_view, login_view,logout_view
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', car_view, name='cars_list'),
-    path('new_car/',new_car_view, name='new_car'),
+    path('login/',login_view, name='login'),
+    path('logaut/',logout_view, name='Logout'),
     path('register/', register_view, name='register'),
+    path('new_car/',new_car_view, name='new_car'),
+    path('cars/', car_view, name='cars_list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
